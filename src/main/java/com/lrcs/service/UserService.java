2861d1aefe1f0d8a8bdc8a2b09429db1193775b8
Luan Rafael <luanraffaell@hotmail.com>
2023-01-18 05:46:44 -0300

    implementing oAuth2 authorization server

--- src/main/java/com/lrcs/service/UserService.java
+++ src/main/java/com/lrcs/service/UserService.java
@@ -3,6 +3,9 @@ package com.lrcs.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,7 @@ import com.lrcs.repository.UserRepository;
 import com.lrcs.service.exceptions.EntityNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private UserRepository userRepository;
 	private BCryptPasswordEncoder passwordEncoder;
@@ -41,5 +44,14 @@ public class UserService {
 	
 	public void copyToEntity(UserDTO dto, User user) {
 		user.setEmail(dto.getEmail());
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("User not found!");
+		}
+		return user;
 	}
 }
